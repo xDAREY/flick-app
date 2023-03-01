4890e4b9c3e79daafad4b283955a5d08625ef47d
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-03-01 07:33:38 +0100

    moviePage

---
+++ app/src/main/java/com/example/flick_app/moviePage.java
@@ -0,0 +1,14 @@
+package com.example.flick_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class moviePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_page);
+    }
+}
